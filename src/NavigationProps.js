38a5559536ac58640dabbc6c557f56bbd8a703b3
Dejan Posavljak <dejan.posavljak@gmail.com>
2017-10-12 17:11:29 -0700

    Adding a comment to the NavigationProps class

--- src/NavigationProps.js
+++ src/NavigationProps.js
@@ -1,6 +1,8 @@
 // @flow
 'use strict'
 
+// Used because React Navigation is not checked by flow
+// Should be extended by props classes in navigation components
 export default class NavigationProps {
   navigation: any;
 }
