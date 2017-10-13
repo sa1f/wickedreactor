4484158547d0223b13ab40574aa907661ecfeeec
Dejan Posavljak <dejan.posavljak@gmail.com>
2017-10-12 20:47:50 -0700

    Adding house markers to the MapScreen

--- src/NavigationProps.js
+++
@@ -1,8 +0,0 @@
-// @flow
-'use strict'
-
-// Used because React Navigation is not checked by flow
-// Should be extended by props classes in navigation components
-export default class NavigationProps {
-  navigation: any;
-}
--- src/components/MapScreen.js
+++ src/components/MapScreen.js
@@ -2,20 +2,37 @@
 'use strict'
 
 import { Component } from 'react';
+import House from '../models/House';
 import { Icon } from 'react-native-elements';
+import { List } from 'immutable';
 import MapView from 'react-native-maps';
-import NavigationProps from '../NavigationProps';
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import { Text } from 'react-native';
 import { View } from 'react-native';
 
-export default class MapScreen extends React.Component<NavigationProps> {
+type Props = {
+  navigation: any;
+}
+
+type State = {
+  houses: List<House>;
+}
+
+export default class MapScreen extends React.Component<Props, State> {
   _vancouverRegion = {
     latitude: 49.2827,
     longitude: -123.1207,
     latitudeDelta: 0.1,
     longitudeDelta: 0.1,
   };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      houses: List(),
+    }
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -24,7 +41,20 @@ export default class MapScreen extends React.Component<NavigationProps> {
       <View style={styles.container}>
         <MapView
           region={this._vancouverRegion}
-          style={styles.map} />
+          style={styles.map}>
+          {this.state.houses.map((house, index) => (
+            <MapView.Marker
+              coordinate={{
+                latitude: house.getLatitude(),
+                longitude: house.getLongitude(),
+              }}
+              key={index}>
+              <MapView.Callout>
+                <Text>{house._price}</Text>
+              </MapView.Callout>
+            </MapView.Marker>
+          ))}
+        </MapView>
         <Icon
           name='filter'
           type='font-awesome'
