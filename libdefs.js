1c7baf40f51f19bfdc03b19a6aae563dd5785713
Raj Kambo <raj.kambo@outlook.com>
2017-11-07 16:53:55 -0800

    Travis was failing due to missing libdefs module.. this should fix the issue.

--- libdefs.js
+++ libdefs.js
@@ -2,3 +2,4 @@ declare module 'react-native' { declare var exports: any; }
 declare module 'react-navigation' { declare var exports: any; }
 declare module 'react-native-settings-list' { declare var exports: any; }
 declare module 'react-native-prompt' { declare var exports: any; }
+declare module 'react-native-card-view' { declare var exports: any; }
