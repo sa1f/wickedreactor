e3472303570949a02f07364f861501581e2f1d58
Raj Kambo <raj.kambo@outlook.com>
2017-11-07 17:16:27 -0800

    Added dependency for react-native-button

--- libdefs.js
+++ libdefs.js
@@ -3,3 +3,4 @@ declare module 'react-navigation' { declare var exports: any; }
 declare module 'react-native-settings-list' { declare var exports: any; }
 declare module 'react-native-prompt' { declare var exports: any; }
 declare module 'react-native-card-view' { declare var exports: any; }
+declare module 'react-native-button' {declare var exports: any; }
